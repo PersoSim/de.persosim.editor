98503d73167db1816e892d19e0e52956d4e42ae9
Martin Boonk <martin.boonk@secunet.com>
2018-02-28 15:23:32 +0100

    Adds a simple about dialog

---
+++ de.persosim.editor.ui/src/de/persosim/editor/ui/editor/AboutDialog.java
@@ -0,0 +1,63 @@
+package de.persosim.editor.ui.editor;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Shell;
+
+public class AboutDialog extends Dialog {
+
+	public static final String PERSOSIM_URL = "http://www.persosim.de";
+
+	private static final String FONT_NAME = "Helvetica";
+
+	public AboutDialog(Shell parentShell) {
+		super(parentShell);
+
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parentComposite) {
+		final Composite parent = parentComposite;
+
+		parent.setLayout(new GridLayout(1, false));
+
+		Composite container = (Composite) super.createDialogArea(parent);
+		container.setLayout(new GridLayout(1, true));
+
+		Label label2 = new Label(container, SWT.NONE);
+		label2.setText("PersoSim Editor");
+		label2.setFont(new Font(container.getDisplay(), FONT_NAME, 24, SWT.BOLD));
+
+		Label label3 = new Label(container, SWT.NONE);
+		label3.setText(getProductVersion());
+		label3.setFont(new Font(container.getDisplay(), FONT_NAME, 12, SWT.NONE));
+
+		Link link = new Link(container, SWT.NONE);
+		link.setText("Visit our web site at " + PERSOSIM_URL);
+		link.setSize(link.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		
+		return container;
+	}
+
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("About PersoSim Editor");
+	}
+
+	/**
+	 * This method returns the version as a String
+	 * 
+	 * @return version
+	 */
+	public String getProductVersion() {
+		return "Beta 4";
+	}
+
+}
--- de.persosim.editor.ui/src/de/persosim/editor/ui/launcher/StandaloneLauncher.java
+++ de.persosim.editor.ui/src/de/persosim/editor/ui/launcher/StandaloneLauncher.java
@@ -2,14 +2,22 @@ package de.persosim.editor.ui.launcher;
 
 import java.nio.file.Paths;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.Shell;
 import org.globaltester.cryptoprovider.Crypto;
 import org.globaltester.cryptoprovider.bc.ProviderBc;
 
+import de.persosim.editor.ui.editor.AboutDialog;
 import de.persosim.editor.ui.editor.ConfigurationConstants;
 import de.persosim.editor.ui.editor.IniPreferenceStoreAccessor;
 import de.persosim.editor.ui.editor.PersoEditorView;
+import de.persosim.editor.ui.editor.SignatureSettingsDialog;
 import de.persosim.simulator.cardobjects.MasterFile;
 import de.persosim.simulator.cardobjects.TrustPointCardObject;
 import de.persosim.simulator.cardobjects.TrustPointIdentifier;
@@ -17,6 +25,17 @@ import de.persosim.simulator.crypto.certificates.CardVerifiableCertificate;
 import de.persosim.simulator.exception.AccessDeniedException;
 import de.persosim.simulator.exception.CertificateNotParseableException;
 import de.persosim.simulator.perso.DefaultPersonalization;
+import de.persosim.simulator.perso.Personalization;
+import de.persosim.simulator.perso.Profile01;
+import de.persosim.simulator.perso.Profile02;
+import de.persosim.simulator.perso.Profile03;
+import de.persosim.simulator.perso.Profile04;
+import de.persosim.simulator.perso.Profile05;
+import de.persosim.simulator.perso.Profile06;
+import de.persosim.simulator.perso.Profile07;
+import de.persosim.simulator.perso.Profile08;
+import de.persosim.simulator.perso.Profile09;
+import de.persosim.simulator.perso.Profile10;
 import de.persosim.simulator.preferences.PersoSimPreferenceManager;
 import de.persosim.simulator.protocols.ta.TerminalType;
 import de.persosim.simulator.tlv.ConstructedTlvDataObject;
@@ -34,8 +53,98 @@ public class StandaloneLauncher {
 
 		Display display = new Display();
 		Shell shell = new Shell(display);
+		shell.setText("PersoSim Editor");
+
 		PersoEditorView editor = new PersoEditorView();
-		editor.createPartControl(shell);
+		
+		Menu topLevelMenu = new Menu(shell, SWT.BAR);
+		MenuItem fileItem = new MenuItem(topLevelMenu, SWT.CASCADE);
+		fileItem.setText("File");
+		MenuItem settingsItem = new MenuItem(topLevelMenu, SWT.CASCADE);
+		settingsItem.setText("Settings");
+		MenuItem profilesItem = new MenuItem(topLevelMenu, SWT.CASCADE);
+		profilesItem.setText("Profiles");
+		MenuItem aboutItem = new MenuItem(topLevelMenu, SWT.CASCADE);
+		aboutItem.setText("About");
+		
+		Menu fileMenu = new Menu(topLevelMenu);
+		fileItem.setMenu(fileMenu);
+
+		MenuItem open = new MenuItem(fileMenu, SWT.NONE);
+		open.setText("Open");
+		open.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fd = new FileDialog(Display.getDefault().getActiveShell(), SWT.OPEN);
+				fd.setText("Open");
+				fd.setFilterPath("C:/");
+				String[] filterExt = { "*.perso", "*.*" };
+				fd.setFilterExtensions(filterExt);
+				String selection = fd.open();
+				if (selection != null) {
+					editor.updateContent(Paths.get(selection));
+				}
+			}
+		});
+		
+		MenuItem saveas = new MenuItem(fileMenu, SWT.NONE);
+		saveas.setText("Save as...");
+		saveas.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				FileDialog fd = new FileDialog(Display.getDefault().getActiveShell(), SWT.SAVE);
+				fd.setText("Open");
+				fd.setFilterPath(editor.getPath() == null ? "C:/" : editor.getPath().toString());
+				String[] filterExt = { "*.perso", "*.*" };
+				fd.setFilterExtensions(filterExt);
+				String selection = fd.open();
+				if (selection != null) {
+					editor.save(Paths.get(selection));
+				}
+			}
+		});
+
+		Menu settingsMenu = new Menu(topLevelMenu);
+		settingsItem.setMenu(settingsMenu);
+		
+		MenuItem signingItem = new MenuItem(settingsMenu, SWT.NONE);
+		signingItem.setText("Signature settings");
+		signingItem.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				new SignatureSettingsDialog(shell).open();
+			}
+		});
+
+		Menu profilesMenu = new Menu(topLevelMenu);
+		profilesItem.setMenu(profilesMenu);
+		
+		for (Personalization perso : new Personalization [] {new Profile01(), new Profile02(), new Profile03(), new Profile04(), new Profile05(), new Profile06(), new Profile07(), new Profile08(), new Profile09(), new Profile10()}){
+			MenuItem profileItem = new MenuItem(profilesMenu, SWT.NONE);
+			profileItem.setText(perso.getClass().getSimpleName());
+			profileItem.addSelectionListener(new SelectionAdapter() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					editor.updateContent(perso);
+				}
+			});
+		}
+
+		Menu aboutMenu = new Menu(topLevelMenu);
+		aboutItem.setMenu(aboutMenu);
+		
+		MenuItem aboutItem2 = new MenuItem(aboutMenu, SWT.NONE);
+		aboutItem2.setText("About");
+		aboutItem2.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				new AboutDialog(shell).open();
+			}
+		});
+		
+		shell.setMenuBar(topLevelMenu);
+		
+		editor.createEditor(shell);
 
 		editor.updateContent(new DefaultPersonalization() {
 			@Override
@@ -48,6 +157,8 @@ public class StandaloneLauncher {
 				mf.addChild(trustPointAt);
 			}
 		});
+				
+		
 		shell.open();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch()) {
