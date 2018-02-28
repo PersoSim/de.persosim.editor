c8f30e16045b2778087593b38c30770f985a0abf
Martin Boonk <martin.boonk@secunet.com>
2018-02-28 10:10:34 +0100

    Check hex format by trying to parse the string

--- de.persosim.editor.ui/src/de/persosim/editor/ui/editor/checker/HexChecker.java
+++ de.persosim.editor.ui/src/de/persosim/editor/ui/editor/checker/HexChecker.java
@@ -3,6 +3,7 @@ package de.persosim.editor.ui.editor.checker;
 import org.eclipse.swt.widgets.Text;
 
 import de.persosim.editor.ui.editor.checker.FieldCheckResult.State;
+import de.persosim.simulator.utils.HexString;
 
 public class HexChecker implements TextFieldChecker {
 
@@ -11,8 +12,10 @@ public class HexChecker implements TextFieldChecker {
 		if (field.getText().length() % 2 == 1){
 			return new FieldCheckResult("Hex string needs to be of even length", State.ERROR);
 		}
-		if (!field.getText().matches("[a-fA-F0-9\\s]*")){
-			return new FieldCheckResult("Hex string can only contain letters, digits and whitespace", State.WARNING);
+		try {
+			HexString.toByteArray(field.getText());
+		} catch (Exception e){
+			return new FieldCheckResult("Not a valid hexadecimal string", State.ERROR);
 		}
 		return FieldCheckResult.OK;
 	}
